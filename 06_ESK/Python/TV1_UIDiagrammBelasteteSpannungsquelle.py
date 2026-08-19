def830e6381df53ab8538b3472da9742c47a39e6
josamue <149506983+josamue@users.noreply.github.com>
2026-08-19 10:27:15 +0200

    Done

--- 06_ESK/Python/TV1_UIDiagrammBelasteteSpannungsquelle.py
+++ 06_ESK/Python/TV1_UIDiagrammBelasteteSpannungsquelle.py
@@ -2,31 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import ceil
 
-current = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4.0, 4.5, 5])
-dI = 0
+current = np.array([10.67, 12.6, 14.6, 16.6, 18.6, 20.6, 22.6, 24.6, 26.6, 28.6, 30.6, 33.2])
+dI = np.array([0.15, 0.6, 0.6, 0.6, 0.6, 0.7, 0.7, 0.7, 0.7, 0.7, 0.8, 0.8])
 
-voltage = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-dU = 0
+voltage = np.array([1.362, 1.335, 1.290, 1.259, 1.223, 1.186, 1.145, 1.104, 1.064, 1.017, 1.002, 0.938])
+dU = np.array([0.018, 0.018, 0.017, 0.017, 0.017, 0.016, 0.016, 0.015, 0.015, 0.015, 0.015, 0.014])
 
-line, cov = np.polyfit(voltage, current, 1, cov = True)
+line, cov = np.polyfit(current, voltage, 1, cov = True)
 slope, b = line
 dSlope, dB = np.sqrt(np.diag(cov))
 
 fig, ax = plt.subplots(figsize = (7, 4.5))
-ax.errorbar(voltage, current, dI, dU,
+ax.errorbar(current, voltage, dU, dI,
             fmt='o', color='steelblue', ecolor='salmon',
             ms=3, capsize=3, label='Messdaten'
 )
-ax.plot(voltage, slope*voltage + b,
+ax.plot(current, slope*current + b,
         color = "red", linestyle = "--",
         label = rf'Linearer Fit' + '\n' + 
-                rf'$m = ({round(slope * 10**2)/10**2:.2f} \pm {ceil(dSlope * 10**2)/10**2})$ A/V' +
+                rf'$m = ({round(slope * 10**4)/10**4} \pm {ceil(dSlope * 10**4)/10**4})$ V/mA' +
                 '\n' +
-                rf'$b = ({round(b * 10)/10} \pm {ceil(dB * 10)/10})$ A'
+                rf'$b = ({round(b * 10**3)/10**3} \pm {ceil(dB * 10**3)/10**3})$ V'
 )
-ax.set_ylabel(r"$I/(\mathrm{A})$")
-ax.set_xlabel(r"$U/(\mathrm{V})$ ")
+ax.set_xlabel(r"$I/(\mathrm{mA})$")
+ax.set_ylabel(r"$U/(\mathrm{V})$ ")
 ax.legend()
-ax.set_title(r"$I(U)$-Diagramm zur Überprüfung des Ohm'schen Gesetzes")
-plt.show()
-# plt.savefig("./05_ESK/Images/TV2_U-I-Diagramm.jpg", dpi = 400)
+ax.set_title(r"$U(I)$-Diagramm einer belateten galvanischen Zelle")
+# plt.show()
+plt.savefig("./06_ESK/Images/TV1_U-I-Diagramm.jpg", dpi = 400)
--- 06_ESK/Python/TV2_IUDiagramm.py
+++ 06_ESK/Python/TV2_IUDiagramm.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import ceil
 
-current = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4.0, 4.5, 5])
-dI = 0
+current = np.array([0.59, 1.21, 1.82, 2.43, 3.04, 3.65, 4.27, 4.87, 5.49])
+dI = 0.01 * current + 0.05
 
-voltage = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-dU = 0
+voltage = np.array([1.99, 4.01, 6.01, 8.00, 10.00, 12.01, 14.05, 16.01, 18.03])
+dU = 0.009 * voltage + 0.04
 
 line, cov = np.polyfit(voltage, current, 1, cov = True)
 slope, b = line
@@ -20,13 +20,13 @@ ax.errorbar(voltage, current, dI, dU,
 ax.plot(voltage, slope*voltage + b,
         color = "red", linestyle = "--",
         label = rf'Linearer Fit' + '\n' + 
-                rf'$m = ({round(slope * 10**2)/10**2:.2f} \pm {ceil(dSlope * 10**2)/10**2})$ A/V' +
+                rf'$m = ({round(slope * 10**5)/10**5:.5f} \pm {ceil(dSlope * 10**5)/10**5})$ mA/V' +
                 '\n' +
-                rf'$b = ({round(b * 10)/10} \pm {ceil(dB * 10)/10})$ A'
+                rf'$b = ({round(b * 10**4)/10**4} \pm {ceil(dB * 10**4)/10**4})$ mA'
 )
 ax.set_ylabel(r"$I/(\mathrm{A})$")
 ax.set_xlabel(r"$U/(\mathrm{V})$ ")
 ax.legend()
 ax.set_title(r"$I(U)$-Diagramm zur Überprüfung des Ohm'schen Gesetzes")
-plt.show()
-# plt.savefig("./05_ESK/Images/TV2_U-I-Diagramm.jpg", dpi = 400)
+# plt.show()
+plt.savefig("./06_ESK/Images/TV2_U-I-Diagramm.jpg", dpi = 400)
